8773a6f7e010a03c81f4b32a373ff6dde0ad53ff
Mickey D. Luffy <thefalconslayer@gmail.com>
2021-11-27 12:46:54 +0000

    Add get subscription and post subscriptions resources

--- src/main/java/com/turntabl/marketdata/resources/MarketDataResource.java
+++ src/main/java/com/turntabl/marketdata/resources/MarketDataResource.java
@@ -1,17 +1,23 @@
 package com.turntabl.marketdata.resources;
 
 import com.turntabl.marketdata.dto.OrderBookDto;
+import com.turntabl.marketdata.service.MarketDataService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/webhook")
+@RequestMapping("/api/v1")
 @Slf4j
+@RequiredArgsConstructor
 public class MarketDataResource {
 
+    private final MarketDataService marketDataService;
 
 //    @RequestMapping(value="/market-data", method= RequestMethod.POST) same as below
     @PostMapping("/market-data")
@@ -19,4 +25,20 @@ public class MarketDataResource {
         log.info("order books: {}" ,orderBooks);
         return ResponseEntity.ok("success!!");
     }
+
+    @GetMapping("/md")
+    ResponseEntity<?> getOrderBooks() {
+        return new ResponseEntity<>(marketDataService.getOrderBooks(), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/md/subscription")
+    ResponseEntity<?> getSubscriptions() {
+      return new ResponseEntity<>(marketDataService.getSubscriptions(), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/md/subscription")
+    ResponseEntity<?> subscribe() {
+       return new ResponseEntity<>("Mickey D Luffy", HttpStatus.ACCEPTED);
+    }
+
 }
