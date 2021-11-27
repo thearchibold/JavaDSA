c3d06e77d88d02109f49cb88a68959e3e88c00dd
Mickey D. Luffy <thefalconslayer@gmail.com>
2021-11-27 12:45:59 +0000

    Add controller advice to intercept 404 exceptions and propagate custom error message

---
+++ src/main/java/com/turntabl/marketdata/confg/MarketDataControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.turntabl.marketdata.confg;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class MarketDataControllerAdvice {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    protected ResponseEntity<String> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>("Resource not found", new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+
+}
