0c4c02eef902c38002b11a5d4cff095b7250b2be
Mickey D. Luffy <thefalconslayer@gmail.com>
2021-11-27 12:51:28 +0000

    Add sbscribe implementation

--- src/main/java/com/turntabl/marketdata/service/impl/MarketDataServiceImpl.java
+++ src/main/java/com/turntabl/marketdata/service/impl/MarketDataServiceImpl.java
@@ -4,10 +4,12 @@ import com.turntabl.marketdata.dto.OrderBookDto;
 import com.turntabl.marketdata.service.MarketDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 @Service
@@ -15,13 +17,29 @@ public class MarketDataServiceImpl implements MarketDataService {
     @Autowired
     private  RestTemplate restTemplate;
 
-    @Value("${market-data.variables.exhange-url}")
+    @Value("${market-data.variables.exchange-url.one}")
     private String exchange1Url;
 
     @Override
     public List<OrderBookDto> getOrderBooks() {
+//        return restTemplate
+//                .getForEntity(exchange1Url, List<OrderBookDto>.class).getBody();
+        return restTemplate.exchange(exchange1Url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<OrderBookDto>>(){}).getBody();
 
-//        restTemplate.exchange(exchange1Url, HttpMethod.GET, )
-        return null;
+    }
+
+    @Override
+    public List<String> getSubscriptions() {
+        return restTemplate.exchange(exchange1Url + "/subscription", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<String>>() { }).getBody();
+    }
+
+    @Override
+    public void subscribe() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "application/json");
+        HttpEntity<?> httpEntity = new HttpEntity<>("", httpHeaders);
+        restTemplate.exchange(exchange1Url + "/subscription", HttpMethod.POST, httpEntity, String.class);
     }
 }
