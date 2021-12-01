63ca2d75de641ff7be3f1bcb9845d2a033f0151e
MickeyDLuffy <thefalconslayer@gmail.com>
2021-12-01 14:03:32 +0000

    REDIS URL CONFIG ADDED

--- src/main/java/com/turntabl/marketdata/config/RedisConfig.java
+++ src/main/java/com/turntabl/marketdata/config/RedisConfig.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -19,13 +20,13 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 public class RedisConfig {
     @Value("${market-data.variables.redis.topic}")
     private  String topic;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-//        JedisConnectionFactory factory = new JedisConnectionFactory();
-//        factory.setHostName(redisHostName);
-//        factory.setPort(redisPort);
-//        factory.setUsePool(true);
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(System.getenv("REDIS_URL"));
+//        redisStandaloneConfiguration.setPassword(RedisPassword.of("yourRedisPasswordIfAny"));
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
