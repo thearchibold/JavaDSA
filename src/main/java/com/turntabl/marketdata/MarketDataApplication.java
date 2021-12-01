a49163b50fb6462d1bc0a1938374480e4a7b8f73
MickeyDLuffy <thefalconslayer@gmail.com>
2021-12-01 11:47:08 +0000

    REDIS URL CONFIG ADDED

--- src/main/java/com/turntabl/marketdata/MarketDataApplication.java
+++ src/main/java/com/turntabl/marketdata/MarketDataApplication.java
@@ -2,18 +2,22 @@ package com.turntabl.marketdata;
 
 import com.turntabl.marketdata.service.MessageListener;
 import com.turntabl.marketdata.service.impl.RedisMessageSubscriber;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Slf4j
 public class MarketDataApplication {
-
+    @Value("${market-data.test}")
+    private static String test;
     public static void main(String[] args) {
         SpringApplication.run(MarketDataApplication.class, args);
-        System.out.println(System.getenv("START"));
+        log.info("This is testing ENVS {}", test);
     }
 
 
