5fb8fe7db3e1d912852c9fb2f8b2a7ab9a986530
MickeyDLuffy <thefalconslayer@gmail.com>
2021-12-02 12:46:38 +0000

    A few items commented out

--- src/main/java/com/turntabl/marketdata/resources/MarketDataResource.java
+++ src/main/java/com/turntabl/marketdata/resources/MarketDataResource.java
@@ -80,31 +80,31 @@ public class MarketDataResource {
 
     }
 
-    @PostMapping("/callback2/webhook")
-    public void onSecondMarketDataSubscribe(@RequestBody String body) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-        OrderBookDto orderBookDto = new OrderBookDto();
-        OrderBookDto orderBookDto2 = new OrderBookDto();
-        orderBookDto.setAskPrice(10f);
-        orderBookDto2.setAskPrice(102f);
-        orderBookDto.setBidPrice(400f);
-        orderBookDto2.setBidPrice(903f);
-        orderBookDto.setTicker("mxn");
-        orderBookDto2.setTicker("dluffy");
-        orderBookDto.setBuyLimit(9875);
-        orderBookDto.setBuyLimit(10000);
-        String arrayToJson = null;
-
-        try {
-            arrayToJson = objectMapper.writeValueAsString(List.of(orderBookDto, orderBookDto2));
-            redisMessagePublisher.publish2(arrayToJson);
-        } catch (JsonProcessingException ex) {
-            System.out.println("Error processing data");
-        }
-
-    }
+//    @PostMapping("/callback2/webhook")
+//    public void onSecondMarketDataSubscribe(@RequestBody String body) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+//
+//        OrderBookDto orderBookDto = new OrderBookDto();
+//        OrderBookDto orderBookDto2 = new OrderBookDto();
+//        orderBookDto.setAskPrice(10f);
+//        orderBookDto2.setAskPrice(102f);
+//        orderBookDto.setBidPrice(400f);
+//        orderBookDto2.setBidPrice(903f);
+//        orderBookDto.setTicker("mxn");
+//        orderBookDto2.setTicker("dluffy");
+//        orderBookDto.setBuyLimit(9875);
+//        orderBookDto.setBuyLimit(10000);
+//        String arrayToJson = null;
+//
+//        try {
+//            arrayToJson = objectMapper.writeValueAsString(List.of(orderBookDto, orderBookDto2));
+//            redisMessagePublisher.publish2(arrayToJson);
+//        } catch (JsonProcessingException ex) {
+//            System.out.println("Error processing data");
+//        }
+//
+//    }
 
 
     @GetMapping("/md")
