3391fa52aeeba0e42edf90f8d6834dd74bf9f9d2
Archibold Bernard <aephisarh@gmail.com>
2023-05-29 08:19:08 +0000

    keep track of all the min guys

--- DSA/BestTimeToBuyStock.java
+++ DSA/BestTimeToBuyStock.java
@@ -1,6 +1,10 @@
 public class BestTimeToBuyStock {
 	public static void main(String[] args) {
 		int[] prices = {7, 1, 5, 3, 6, 4};
+		profitWithLarge(prices);
+	}
+
+	public static int doubleLoop(int[] prices){
 		int[][] sellingMap = new int[prices.length][prices.length];
 		int profit = 0, buyIndex = 0, sellIndex = 0;
 
@@ -10,10 +14,21 @@ public class BestTimeToBuyStock {
 				sellingMap[j][i] = _profit;
 				if(_profit > profit){
 					profit = _profit;
-					buyIndex = j;
-					sellIndex = i;
 				}
 			}
 		}
+		return profit;
+	}
+
+	public static int profitWithLarge(int[] prices){
+		int profit = 0;
+		int minPrice = Integer.MAX_VALUE;
+
+		// so basically keep track of the min price and subtract from all the others.
+		for (int i = 0; i < prices.length; i++) {
+			minPrice = Math.min(minPrice, prices[i]);
+			profit = Math.max(profit, prices[i] - minPrice);
+		}
+		return profit;
 	}
 }
